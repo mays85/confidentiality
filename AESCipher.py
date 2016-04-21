924fb1ff24afcff554b4a947ebded3f458c03247
Anthony Portante <aportante10@gmail.com>
2016-04-21 10:34:05 -0400

    Update AESCipher.py

--- AESCipher.py
+++ AESCipher.py
@@ -9,23 +9,32 @@ http://stackoverflow.com/questions/12524994/encrypt-decrypt-using-pycrypto-aes-2
 from Crypto import Random
 from Crypto.Cipher import AES
 import base64
+import os
 
-BLOCK_SIZE = 32
+KEY_SIZE = 16
+BLOCK_SIZE = 16
 pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * chr(BLOCK_SIZE - len(s) % BLOCK_SIZE)
 unpad = lambda s: s[:-ord(s[len(s)-1:])]
 
 
-#This constructor initializes the AESCipher object and sets the key value for the object. The key is given as an argument 
-#for the constructor.
-
+# This constructor initializes the AESCipher 
+# object and sets the key value for the object.
+# The key is given as an argument 
+# for the constructor.
 class AESCipher:
-	def __init__(self , key):
-		self.key = key
+	def __init__(self , key=None):
+		if key is None:
+			self.key = os.urandom(KEY_SIZE) # generate random key
+		else:
+			self.key = key #using key feed into constructor
+		self.BLOCK_SIZE = BLOCK_SIZE;
+		self.KEY_SIZE = KEY_SIZE
 
 
-#The encrypt function takes the raw movie data and returns the encrypted version using the key value. 
-#Finally, the function encodes the data in base64 encoding -- IS THAT REQUIRED???
 
+# The encrypt function takes the raw movie 
+# data and returns the encrypted version
+# using the key value. 
 	def encrypt (self , raw):
 		raw = pad (raw)
 		iv = Random.new().read( AES.block_size )
