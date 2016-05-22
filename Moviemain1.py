09e89a47e5f55d2216522d307bb13484e67d54d0
Anthony Portante <aportante10@gmail.com>
2016-05-22 14:29:27 -0400

    Update Moviemain1.py

--- Moviemain1.py
+++ Moviemain1.py
@@ -9,17 +9,30 @@ from Crypto.Hash import SHA256
 from AEScipher import *
 from Payload import *
 from socket import *
+import RSA_api
 import cPickle
+
+Client_RSApk = 0
+
+def secure_wipe():
+	print '\n[*] SECURE DESTROY IN PROGRESS'
+	global Client_RSApk
+	srsa = sys.getsizeof(Client_RSApk)
+	Client_RSApk = bytearray(os.urandom(sys.getsizeof(srsa)))
+	print '[*] COMPLETE!'
 
 def parse_args():
 	# MODIFIED ARGUMENTS FROM MOVIEMAIN
-	# for this file, we only need to know which movie file we're going to serve up.
+	# for this file, we only need to know 
+	# which movie file we're going to serve up.
 	parser = argparse.ArgumentParser(description = 'Serve up an encrypted movie')
 	parser.add_argument ('-f', '--file', help = 'file name to be served')
 	args = parser.parse_args()
 	return args
 
 def main():
+	global Client_RSApk
+
 	# parse arguments (that's important)
 	args = parse_args()
 
@@ -45,19 +58,12 @@ def main():
 	print "b64 Key: " + b64KEY + "\n"
 	# use AESCipher object to encrypt raw_data
 	encryptedData = aes.encrypt(raw_data)
-	# encryptedSize = sys.getsizeof(encryptedData)
-	# print "Encrypted data size: %.2f MB" % (encryptedSize / 1000000.0)
+	encryptedSize = sys.getsizeof(encryptedData)
+	print "Encrypted data size: %.2f MB" % (encryptedSize / 1000000.0)
 	
-	# Payload object contains the necessary information for symmetric encryption.
+	# Payload object contains encrypted movie and 
+	# the necessary information for symmetric encryption.
 	pay = Payload(b64KEY, dataHash, encryptedData)
-
-	#using cPickle to serialize the data
-	to_send = cPickle.dumps(pay)
-
-	#because the data is large (movies are big), we need to know how much data we're sending
-	toSize = sys.getsizeof(to_send)
-	print "to_size = %d" %toSize
-	
 
 	# now, create the server socker and listen.
 	serverPort = 12000
@@ -67,20 +73,41 @@ def main():
 
 	# listening for connections
 	while 1:
-		print "The server is ready to receive"
+		#accept inbound tcp connections
+		print "The server is ready to receive connections"
 		connectionSocket, addr = serverSocket.accept()
 
+		#Receive the projectors public key obj
+		print '[*] Receiving projector public RSA key'
+		cRSA = connectionSocket.recv(1024)
+		Client_RSApk = cPickle.loads(cRSA) 
+		#print our obj for verification
+		print '[*] Projector RSA Public Key => ', Client_RSApk
+		
+		pay.key = Client_RSApk.encrypt(pay.key, '')[0]
+		pay.fhash = Client_RSApk.encrypt(pay.fhash, '')[0]
+		to_send = cPickle.dumps(pay)
+		#get size of encrypted and pickled payload obj
+		toSize = sys.getsizeof(to_send)
+		print "Size of Payload = %d Bytes" %toSize
+
 		# client needs to know size of the data.
-		ready = connectionSocket.recv(1024)	
-		if ready is 'y' or 'Y':
-			connectionSocket.send(str(toSize))
+		connectionSocket.send(str(toSize))
+		connectionSocket.recv(1)
 
 		# client is now ready to accept the data.
-		ready = connectionSocket.recv(1024)
-		if ready is 'y' or 'Y':
-			# sending the data
-			connectionSocket.send(to_send)
-
+		connectionSocket.send(to_send)
+		
+		secure_wipe()
+		sys.exit(1)
 
 if __name__ == "__main__":
-	main()
+	try:
+		main()		
+	except KeyboardInterrupt:
+		# if a keyboard interupt is detected, 
+		# we wipe RSA key pair from ram
+		# and tell the OS to clean up our proc
+		print "\n\nDetected Keyboard Interrupt!\n"
+		secure_wipe()
+		sys.exit(1)
