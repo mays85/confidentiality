80e095a815d0fb65e8fb3ed78014db5854f0a0a8
mays85 <calebmays85@gmail.com>
2016-06-01 09:55:40 -0400

    Update Payload.py

--- Payload.py
+++ Payload.py
@@ -1,3 +1,11 @@
+'''
+This file is the class that defines what the payload for this system is.
+Our payload is:
+- The AES encryted data of the movie file for ensuring confidentiality.
+- The RSA encrypted AES key for the decrypting the file for ensuring authentiation.
+- THe RSA encrypted SHA hash for ensuring integrity.
+'''
+
 import base64
 import os
 
